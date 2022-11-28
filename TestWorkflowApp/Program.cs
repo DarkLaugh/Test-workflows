46aaf07286739e1cb2948dfa4666b3f06c0e0d1b
Gencho Draganov <gdrag@softserveinc.com>
2022-11-28 13:33:20 +0200

    Added Test Console App

---
+++ TestWorkflowApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestWorkflowApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var env = args[0];
+            var specificDatabaseName = args[1];
+
+            Console.WriteLine($"Executing on {env} environment. Running migrations on - {specificDatabaseName} database.");
+        }
+    }
+}
