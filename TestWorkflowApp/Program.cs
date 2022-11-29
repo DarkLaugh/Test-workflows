b9e277199cfb8c61cfb5636b7caef4e6975a9e59
Gencho Draganov <gdrag@softserveinc.com>
2022-11-29 17:58:51 +0200

    Updated console app

--- TestWorkflowApp/Program.cs
+++ TestWorkflowApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TestWorkflowApp
+﻿using System.Data.SqlTypes;
+
+namespace TestWorkflowApp
 {
     internal class Program
     {
@@ -6,6 +8,12 @@
         {
             var env = args[0];
             var specificDatabaseName = args[1];
+            bool.TryParse(args[2], out bool throwException);
+
+            if (throwException)
+            {
+                throw new SqlNullValueException("Missing value for ID column");
+            }
 
             Console.WriteLine($"Executing on {env} environment. Running migrations on - {specificDatabaseName} database.");
         }
